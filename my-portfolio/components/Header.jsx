0711c91c964fd24066a42d7a71f2fae572fa37f5
nhatminh4401 <58182906+nhatminh4401@users.noreply.github.com>
2024-06-19 23:22:22 +0700

    feat: Add navigation

--- my-portfolio/components/Header.jsx
+++ my-portfolio/components/Header.jsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-      <div className="container mx-auto">header</div>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"}>
+          <h1 className="text-4xl font-semibold">
+            Minh<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* Desktop navigation */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href={"/contact"}>
+            <Button>Contact</Button>
+          </Link>
+        </div>
+
+        {/* Mobile navigation */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
     </header>
   );
 };
