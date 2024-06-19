a22010757c0c50d9714d7c9290bd74f6924eead3
nhatminh4401 <58182906+nhatminh4401@users.noreply.github.com>
2024-06-19 23:56:27 +0700

    feat: Add transition while navigating

--- my-portfolio/app/layout.jsx
+++ my-portfolio/app/layout.jsx
@@ -1,6 +1,8 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrains_Mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -18,7 +20,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetBrains_Mono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
